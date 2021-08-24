c3391e84fafd398d00a9ac2b92cbe8c8a85e1aad
yeasinopu17 <yeasinopu@gmail.com>
2021-08-24 22:06:12 +0600

    before past event

--- functions.php
+++ functions.php
@@ -6,8 +6,7 @@
 //   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 //   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 // }
-function university_files()
-{
+function university_files() {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
@@ -28,8 +27,7 @@ function university_files()
 add_action('wp_enqueue_scripts', 'university_files');
 
 
-function university_features()
-{
+function university_features() {
   // register_nav_menu('headerMenuLocation', 'Header Menu Location');
   // register_nav_menu('footerLocationOne', 'Footer Location One');// create location of menu in admin panel
   // register_nav_menu('footerLocationTwo', 'Footer Location Two');
@@ -37,5 +35,23 @@ function university_features()
 }
 
 
-add_action('after_setup_theme', 'university_features');// for title appear in every page
+add_action('after_setup_theme', 'university_features'); // for title appear in every page
 
+
+function university_adjust_queries($query) {
+  if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+      array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    ));
+  }
+}
+add_action('pre_get_posts', 'university_adjust_queries');
