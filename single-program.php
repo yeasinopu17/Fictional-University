e68728760b4407e9c832a2610b804779036d80bf
yeasinopu17 <yeasinopu@gmail.com>
2021-08-26 14:39:45 +0600

    Featured Image Sizes & Cropping

--- single-program.php
+++ single-program.php
@@ -49,7 +49,7 @@ while (have_posts()) {
             <li class="professor-card__list-item">
 
               <a class="professor-card" href="<?php the_permalink(); ?>">
-                <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>" alt="">
+                <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscrape'); ?>" alt="">
                 <span class="professor-card__name"><?php the_title(); ?></span>
               </a>
 
