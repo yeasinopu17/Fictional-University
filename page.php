56898517ea696bfa745033a9613afa6ffa9ec4e0
yeasinopu17 <yeasinopu@gmail.com>
2021-08-27 10:32:51 +0600

    Reduce Duplicate Code

--- page.php
+++ page.php
@@ -3,17 +3,9 @@
 get_header();
 
 while (have_posts()) {
-  the_post(); ?>
-
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>Don't forget to replace me later.</p>
-      </div>
-    </div>
-  </div>
+  the_post();
+  pageBanner();
+?>
 
   <div class="container container--narrow page-section">
 
@@ -30,29 +22,28 @@ while (have_posts()) {
     ?>
 
 
-    <?php 
+    <?php
     $testArray = get_pages(array(
-      'child_of' => get_the_ID()// if the current page has child the return an array with child, else return null
+      'child_of' => get_the_ID() // if the current page has child the return an array with child, else return null
     ));
     if ($theParent or $testArray) { ?>
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2><!-- $theParent = 0 then title function return current page title -->
-      <ul class="min-list">
-        <?php 
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2><!-- $theParent = 0 then title function return current page title -->
+        <ul class="min-list">
+          <?php
           if ($theParent) { // if parent
             $findTheChildOf = $theParent;
           } else { // not parent
             $findTheChildOf = get_the_ID();
-            
           }
           wp_list_pages(array(
             'title_li' => NULL,
             'child_of' => $findTheChildOf,
             'sort_column' => 'menu_order'
-          )); 
-        ?>
-      </ul>
-    </div>
+          ));
+          ?>
+        </ul>
+      </div>
     <?php } ?>
 
     <div class="generic-content">
