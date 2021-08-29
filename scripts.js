be3aaf03294e65a3ca521b0e642c8758e0051587
yeasinopu17 <yeasinopu@gmail.com>
2021-08-29 23:37:01 +0600

    complete live search

--- scripts.js
+++ scripts.js
@@ -153,7 +153,12 @@
       this.openButton = $(".js-search-trigger");
       this.closeButton = $(".search-overlay__close");
       this.searchOverlay = $(".search-overlay");
+      this.searchField = $("#search-term");
+      this.resultDiv = $("#search-overlay__results");
       this.isOverlayOpen = false;
+      this.isSpinnerVisible = false;
+      this.typingTimer;
+      this.previousValue;
       this.events();
     }
 
@@ -162,11 +167,36 @@
       this.openButton.on("click", this.openOverlay.bind(this));
       this.closeButton.on("click", this.closeOverlay.bind(this));
       $(document).on("keydown", this.keyPressDispatcher.bind(this));
+      this.searchField.on("keyup", this.typingLogic.bind(this));
     }
 
     // 3. methods (function, action...)
+    typingLogic() {
+      if (this.searchField.val() != this.previousValue) {
+        clearTimeout(this.typingTimer);
+        
+        if (this.searchField.val()) {
+          if (!this.isSpinnerVisible) {
+            this.resultDiv.html('<div class="spinner-loader"></div>');
+            this.isSpinnerVisible = true;
+          }
+          this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        } else {
+          this.resultDiv.html("");
+          this.isSpinnerVisible = false;
+        }
+      }
+
+      this.previousValue = this.searchField.val();
+    }
+
+    getResults() {
+      this.resultDiv.html("Imagin real search result");
+      this.isSpinnerVisible = false;
+    }
+
     keyPressDispatcher(e) {
-      if (e.keyCode == 83 && !this.isOverlayOpen) {
+      if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(":focus")) {
         this.openOverlay();
       }
       if (e.keyCode == 27 && this.isOverlayOpen) {
