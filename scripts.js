8a386e7cddd65771cccb7390c022cbe83efe693d
yeasinopu17 <yeasinopu@gmail.com>
2021-08-29 18:10:11 +0600

    Keyboard Events in JavaScript

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
-(function($) {
+(function ($) {
   /* ===============================================================
     Mobile Menu Feature
   =============================================================== */
   class MobileMenu {
     constructor() {
-      this.menu = document.querySelector(".site-header__menu")
-      this.openButton = document.querySelector(".site-header__menu-trigger")
-      this.events()
+      this.menu = document.querySelector(".site-header__menu");
+      this.openButton = document.querySelector(".site-header__menu-trigger");
+      this.events();
     }
-  
+
     events() {
-      this.openButton.addEventListener("click", () => this.openMenu())
+      this.openButton.addEventListener("click", () => this.openMenu());
     }
-  
+
     openMenu() {
-      this.openButton.classList.toggle("fa-bars")
-      this.openButton.classList.toggle("fa-window-close")
-      this.menu.classList.toggle("site-header__menu--active")
+      this.openButton.classList.toggle("fa-bars");
+      this.openButton.classList.toggle("fa-window-close");
+      this.menu.classList.toggle("site-header__menu--active");
     }
   }
-  
-  new MobileMenu()
-  
+
+  new MobileMenu();
+
   /* ===============================================================
     Hero Slider / Homepage Slideshow
   =============================================================== */
@@ -29,135 +29,179 @@
     constructor() {
       if (document.querySelector(".hero-slider")) {
         // count how many slides there are
-        const dotCount = document.querySelectorAll(".hero-slider__slide").length
-  
+        const dotCount = document.querySelectorAll(
+          ".hero-slider__slide"
+        ).length;
+
         // Generate the HTML for the navigation dots
-        let dotHTML = ""
+        let dotHTML = "";
         for (let i = 0; i < dotCount; i++) {
-          dotHTML += `<button class="slider__bullet glide__bullet" data-glide-dir="=${i}"></button>`
+          dotHTML += `<button class="slider__bullet glide__bullet" data-glide-dir="=${i}"></button>`;
         }
-  
+
         // Add the dots HTML to the DOM
-        document.querySelector(".glide__bullets").insertAdjacentHTML("beforeend", dotHTML)
-  
+        document
+          .querySelector(".glide__bullets")
+          .insertAdjacentHTML("beforeend", dotHTML);
+
         // Actually initialize the glide / slider script
         var glide = new Glide(".hero-slider", {
           type: "carousel",
           perView: 1,
-          autoplay: 3000
-        })
-  
-        glide.mount()
+          autoplay: 3000,
+        });
+
+        glide.mount();
       }
     }
   }
-  
-  new HeroSlider()
-  
+
+  new HeroSlider();
+
   /* ===============================================================
     Google Map / Campus Map
   =============================================================== */
   class GoogleMap {
     constructor() {
-      document.querySelectorAll(".acf-map").forEach(el => {
-        this.new_map(el)
-      })
+      document.querySelectorAll(".acf-map").forEach((el) => {
+        this.new_map(el);
+      });
     }
-  
+
     new_map($el) {
-      var $markers = $el.querySelectorAll(".marker")
-  
+      var $markers = $el.querySelectorAll(".marker");
+
       var args = {
         zoom: 16,
         center: new google.maps.LatLng(0, 0),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-  
-      var map = new google.maps.Map($el, args)
-      map.markers = []
-      var that = this
-  
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+      };
+
+      var map = new google.maps.Map($el, args);
+      map.markers = [];
+      var that = this;
+
       // add markers
       $markers.forEach(function (x) {
-        that.add_marker(x, map)
-      })
-  
+        that.add_marker(x, map);
+      });
+
       // center map
-      this.center_map(map)
+      this.center_map(map);
     } // end new_map
-  
+
     add_marker($marker, map) {
-      var latlng = new google.maps.LatLng($marker.getAttribute("data-lat"), $marker.getAttribute("data-lng"))
-  
+      var latlng = new google.maps.LatLng(
+        $marker.getAttribute("data-lat"),
+        $marker.getAttribute("data-lng")
+      );
+
       var marker = new google.maps.Marker({
         position: latlng,
-        map: map
-      })
-  
-      map.markers.push(marker)
-  
+        map: map,
+      });
+
+      map.markers.push(marker);
+
       // if marker contains HTML, add it to an infoWindow
       if ($marker.innerHTML) {
         // create info window
         var infowindow = new google.maps.InfoWindow({
-          content: $marker.innerHTML
-        })
-  
+          content: $marker.innerHTML,
+        });
+
         // show info window when marker is clicked
         google.maps.event.addListener(marker, "click", function () {
-          infowindow.open(map, marker)
-        })
+          infowindow.open(map, marker);
+        });
       }
     } // end add_marker
-  
+
     center_map(map) {
-      var bounds = new google.maps.LatLngBounds()
-  
+      var bounds = new google.maps.LatLngBounds();
+
       // loop through all markers and create bounds
       map.markers.forEach(function (marker) {
-        var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng())
-  
-        bounds.extend(latlng)
-      })
-  
+        var latlng = new google.maps.LatLng(
+          marker.position.lat(),
+          marker.position.lng()
+        );
+
+        bounds.extend(latlng);
+      });
+
       // only 1 marker?
       if (map.markers.length == 1) {
         // set center of map
-        map.setCenter(bounds.getCenter())
-        map.setZoom(16)
+        map.setCenter(bounds.getCenter());
+        map.setZoom(16);
       } else {
         // fit to bounds
-        map.fitBounds(bounds)
+        map.fitBounds(bounds);
       }
     } // end center_map
   }
-  
-  new GoogleMap()
-  
+
+  new GoogleMap();
+
   /* ===============================================================
     Search / Live Overlay Results
   =============================================================== */
   class Search {
-    
+    // 1. describe and create/initiate our object
+    constructor() {
+      this.openButton = $(".js-search-trigger");
+      this.closeButton = $(".search-overlay__close");
+      this.searchOverlay = $(".search-overlay");
+      this.isOverlayOpen = false;
+      this.events();
+    }
+
+    // 2. events
+    events() {
+      this.openButton.on("click", this.openOverlay.bind(this));
+      this.closeButton.on("click", this.closeOverlay.bind(this));
+      $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    }
+
+    // 3. methods (function, action...)
+    keyPressDispatcher(e) {
+      if (e.keyCode == 83 && !this.isOverlayOpen) {
+        this.openOverlay();
+      }
+      if (e.keyCode == 27 && this.isOverlayOpen) {
+        this.closeOverlay();
+      }
+    }
+
+    openOverlay() {
+      this.searchOverlay.addClass("search-overlay--active");
+      $("body").addClass("body-no-scroll");
+      this.isOverlayOpen = true;
+      console.log("openOverlay run");
+    }
+
+    closeOverlay() {
+      this.searchOverlay.removeClass("search-overlay--active");
+      $("body").removeClass("body-no-scroll");
+      this.isOverlayOpen = false;
+      console.log("closeOverlay run");
+    }
   }
-  
-  new Search()
-  
+
+  new Search();
+
   /* ===============================================================
     My Notes Feature
   =============================================================== */
-  class MyNotes {
-    
-  }
-  
-  new MyNotes()
-  
+  class MyNotes {}
+
+  new MyNotes();
+
   /* ===============================================================
     Like a Professor Feature
   =============================================================== */
-  class Like {
-    
-  }
-  
-  new Like()
-  })(jQuery)
+  class Like {}
+
+  new Like();
+})(jQuery);
